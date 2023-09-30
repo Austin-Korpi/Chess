88a1157da8dd0452745dc6c6523a354e5a9adcf8
Austin Korpi <korpi052@umn.edu>
2023-09-30 16:50:19 -0500

    Fixing include error

--- imgui/imgui_impl_glfw.cpp
+++ imgui/imgui_impl_glfw.cpp
@@ -84,11 +84,11 @@
 #ifdef _WIN32
 #undef APIENTRY
 #define GLFW_EXPOSE_NATIVE_WIN32
-#include <GLFW/glfw3native.h>   // for glfwGetWin32Window()
+#include <glfw3native.h>   // for glfwGetWin32Window()
 #endif
 #ifdef __APPLE__
 #define GLFW_EXPOSE_NATIVE_COCOA
-#include <GLFW/glfw3native.h>   // for glfwGetCocoaWindow()
+#include <glfw3native.h>   // for glfwGetCocoaWindow()
 #endif
 
 #ifdef __EMSCRIPTEN__
